d8783f362006ac7fc3c1340a01b836e224e71a59
Jared Bussell <bussell.jared@gmail.com>
2020-12-08 17:34:16 -0500

    added styling to login page, added bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,18 +3,27 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
-import './../node_modules/bulma/css/bulma.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faEdit } from '@fortawesome/free-solid-svg-icons'
-import { faCat } from '@fortawesome/free-solid-svg-icons'
 import { faDumpsterFire } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
  
-library.add(faUserSecret, faEdit, faCat, faDumpsterFire)
+library.add(faUserSecret, faEdit, faDumpsterFire, faTrash, faCheck, faWindowClose, faLock)
 
  
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
