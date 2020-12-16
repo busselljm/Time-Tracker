7ee38c6b6a8e168bccce6233629472264e3a0069
Winnie Kimani <kimaniwinnie@yahoo.com>
2020-12-16 09:25:17 -0500

    setting up splash page

---
+++ frontend/src/views/Splash.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <splash-page/>
+  </div>
+</template>
+
+<script>
+
+import SplashPage from '@/components/SplashPage.vue';
+
+export default {
+components: {SplashPage},
+}
+</script>
+
+<style>
+
+</style>
